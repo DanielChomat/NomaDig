854791356ee0db28901f73ee121f91f32d1bfe43
Daniel Chomát <daniel@chomat.eu>
2024-09-04 20:22:42 -0400

    Feat: [NOMDIG-6] Connecting i18n

---
+++ src/features/i18n/en.ts
@@ -0,0 +1,13 @@
+export const en = {
+    navigation: {
+        dashboardTabBar: {
+            home: "Home",
+            cowork: "Cowork",
+            finances: "Finances",
+            travel: "Travel",
+            menu: "Menu",
+        }
+    },
+}
+
+export type Translations = typeof en
---
+++ src/features/i18n/es.ts
@@ -0,0 +1,13 @@
+import {Translations} from "./en"
+
+export const es: Translations = {
+    navigation: {
+        dashboardTabBar: {
+            cowork: "Cowork",
+            finances: "Financias",
+            home: "Casa",
+            menu: "Menú",
+            travel: "Viajes",
+        }
+    },
+}
