dea45bd815becb74243ef8d0aa3486353cd2d746
Daniel Chomát <26232812+DanielChomat@users.noreply.github.com>
2024-09-04 20:13:19 -0400

    Feat: [NOMDIG-1]: Starting the Expo Tamagui app (#1)
    * Starting the Expo Tamagui app
    
    * Adding some default screens
    
    * Adding more default Screen stuff

---
+++ src/features/onboarding/screens/ExplanationScreen.tsx
@@ -0,0 +1,8 @@
+import {Text} from "tamagui";
+import {Screen} from "../../../components/Screen";
+
+export const ExplanationScreen = () => {
+    return <Screen>
+        <Text>asdadass</Text>
+    </Screen>
+}
