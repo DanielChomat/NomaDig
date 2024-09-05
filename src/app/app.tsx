8a76658bc31409bc51f136cdb0e86a20cae6defd
Daniel Chomát <26232812+DanielChomat@users.noreply.github.com>
2024-09-05 15:20:05 -0400

    Chore: [NO-NOMDIG] Adding Prettier & ESLint formatting (#5)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced configuration files for Prettier and ESLint to enhance code
    formatting and linting capabilities.
    - Updated `package.json` to streamline formatting scripts and include
    new dependencies for improved code quality.
      
    - **Bug Fixes**
    - Reformatted code in the main application file for better readability
    and consistency without altering functionality.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,88 +1,86 @@
-import {TamaguiProvider, Theme, ThemeName} from "tamagui";
+import { TamaguiProvider, Theme, ThemeName } from "tamagui"
 
-import {useFonts} from "expo-font";
-import {customFontsToLoad} from "../theme/typography";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {createStaticNavigation, StaticParamList} from "@react-navigation/native";
-import {useEffect} from "react";
-import {useIsOnboarded} from "../features/onboarding/hooks/useIsOnboarded";
-import {IntroductionScreen} from "../features/onboarding/screens/IntroductionScreen";
-import {ExplanationScreen} from "../features/onboarding/screens/ExplanationScreen";
-import {config} from "../../tamagui.config";
-import {initialWindowMetrics, SafeAreaProvider} from "react-native-safe-area-context";
-import {useAppColorScheme} from "../hooks/useAppColorScheme";
-import {DefaultScreen} from "../features/dashboard/DefaultScreen";
-import {TabsNavigator} from "./navigators/TabsNavigator";
+import { useFonts } from "expo-font"
 
+import { customFontsToLoad } from "../theme/typography"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createStaticNavigation, StaticParamList } from "@react-navigation/native"
+import { useEffect } from "react"
+import { useIsOnboarded } from "../features/onboarding/hooks/useIsOnboarded"
+import { IntroductionScreen } from "../features/onboarding/screens/IntroductionScreen"
+import { ExplanationScreen } from "../features/onboarding/screens/ExplanationScreen"
+import { config } from "../../tamagui.config"
+import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
+import { useAppColorScheme } from "../hooks/useAppColorScheme"
+import { DefaultScreen } from "../features/dashboard/DefaultScreen"
+import { TabsNavigator } from "./navigators/TabsNavigator"
 
 type NomaDigAppProps = {
-    hideSplashScreen: () => Promise<boolean>
+  hideSplashScreen: () => Promise<boolean>
 }
 
 const RootStack = createNativeStackNavigator({
-    groups: {
-        Onboarding: {
-            if: () => !useIsOnboarded(),
-            screenOptions: {
-                headerShown: false,
-            },
-            screens: {
-                Introduction: {
-                    screen: IntroductionScreen,
-                },
-                Explanation: ExplanationScreen,
-            }
+  groups: {
+    Onboarding: {
+      if: () => !useIsOnboarded(),
+      screenOptions: {
+        headerShown: false,
+      },
+      screens: {
+        Introduction: {
+          screen: IntroductionScreen,
         },
-        App: {
-            if: useIsOnboarded,
-            screenOptions: {
-                headerShown: false,
-            },
-            screens: {
-                DashboardTabs: TabsNavigator,
-            }
-        }
+        Explanation: ExplanationScreen,
+      },
     },
-    screens: {
-        Help: DefaultScreen,
+    App: {
+      if: useIsOnboarded,
+      screenOptions: {
+        headerShown: false,
+      },
+      screens: {
+        DashboardTabs: TabsNavigator,
+      },
     },
-});
+  },
+  screens: {
+    Help: DefaultScreen,
+  },
+})
 
-type RootStackParamList = StaticParamList<typeof RootStack>;
+type RootStackParamList = StaticParamList<typeof RootStack>
 
 declare global {
-    namespace ReactNavigation {
-        interface RootParamList extends RootStackParamList {
-        }
-    }
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
 }
 
-const Navigation = createStaticNavigation(RootStack);
+const Navigation = createStaticNavigation(RootStack)
 
-export const App = ({hideSplashScreen}: NomaDigAppProps) => {
-    const [areFontsLoaded, fontLoadError] = useFonts(customFontsToLoad)
+export const App = ({ hideSplashScreen }: NomaDigAppProps) => {
+  const [areFontsLoaded, fontLoadError] = useFonts(customFontsToLoad)
 
-    const colorScheme = useAppColorScheme();
-    const theme: ThemeName = `${colorScheme}_purple`;
+  const colorScheme = useAppColorScheme()
+  const theme: ThemeName = `${colorScheme}_purple`
 
-
-    useEffect(() => {
-        if (areFontsLoaded && !fontLoadError) {
-            hideSplashScreen()
-        }
-    }, [areFontsLoaded]);
-
-    if (!areFontsLoaded && !fontLoadError) {
-        return null
+  useEffect(() => {
+    if (areFontsLoaded && !fontLoadError) {
+      hideSplashScreen()
     }
+  }, [areFontsLoaded])
 
-    return (
-        <TamaguiProvider config={config} defaultTheme={"dark_purple"}>
-            <Theme name={theme}>
-                <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-                    <Navigation/>
-                </SafeAreaProvider>
-            </Theme>
-        </TamaguiProvider>
-    )
+  if (!areFontsLoaded && !fontLoadError) {
+    return null
+  }
+
+  return (
+    <TamaguiProvider config={config} defaultTheme={"dark_purple"}>
+      <Theme name={theme}>
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <Navigation />
+        </SafeAreaProvider>
+      </Theme>
+    </TamaguiProvider>
+  )
 }
