b67d9e3b0134540398f5b4393e74eb65f69658c7
Daniel Chomát <26232812+DanielChomat@users.noreply.github.com>
2025-05-27 23:35:13 +0200

    Chore: #15 | Implementing ESLint, prettier, tsc, husky & lint-staged tools (#16)
    Resolves #15

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+export default {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+};
